95dcdf7abab531ac0bfd7d955f328277122186ea
amazingguni <amazingguni@gmail.com>
2021-06-24 22:07:48 +0900

    Use unit of work instead of repo adn session

--- src/allocation/service_layer/services.py
+++ src/allocation/service_layer/services.py
@@ -4,6 +4,7 @@ from datetime import date
 from allocation.domain import model
 from allocation.domain.model import OrderLine, Batch
 from allocation.adapters.repository import AbstractRepository
+from allocation.service_layer.unit_of_work import AbstractUnitOfWork
 
 
 class InvalidSku(Exception):
@@ -14,18 +15,19 @@ def is_valid_sku(sku, batches):
     return sku in {b.sku for b in batches}
 
 
-def allocate(order_id: str, sku: str, qty: int,
-             repo: AbstractRepository, session) -> str:
+def allocate(order_id: str, sku: str, qty: int, uow: AbstractUnitOfWork) -> str:
     line = OrderLine(order_id, sku, qty)
-    batches = repo.list()
-    if not is_valid_sku(line.sku, batches):
-        raise InvalidSku(f'Invalid sku {line.sku}')
-    batchref = model.allocate(line, batches)
-    session.commit()
+    with uow:
+        batches = uow.batches.list()
+        if not is_valid_sku(line.sku, batches):
+            raise InvalidSku(f'Invalid sku {line.sku}')
+        batchref = model.allocate(line, batches)
+        uow.commit()
     return batchref
 
 
 def add_batch(ref: str, sku: str, qty: int, eta: Optional[date],
-              repo: AbstractRepository, session) -> None:
-    repo.add(Batch(ref, sku, qty, eta))
-    session.commit()
+              uow: AbstractUnitOfWork) -> None:
+    with uow:
+        uow.batches.add(Batch(ref, sku, qty, eta))
+        uow.commit()
